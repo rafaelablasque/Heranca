8206526b3e9112ce5aa472802ff9178347c4c58f
rafaelablasque <rafaela.blasque@edu.senai.br>
2026-09-17 07:57:42 -0300

    gato

---
+++ Gato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    internal class Gato
+    {
+        public void Miar()
+        {
+            Console.WriteLine("Miau! Miau!");
+        }
+    }
+}
