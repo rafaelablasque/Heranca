62b6e770d718a7ad02470a9d4c3e6bceaee44163
rafaelablasque <rafaela.blasque@edu.senai.br>
2026-09-17 07:52:47 -0300

    Adicionar arquivos de projeto.

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    internal class Animal
+    {
+        public string Nome { get; set; }
+
+        public void Comer()
+        {
+            Console.WriteLine($"{Nome} está comendo");
+        }
+    }
+}
